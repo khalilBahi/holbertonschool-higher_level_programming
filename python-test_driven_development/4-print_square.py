39b5c5f83b2c72bcf71dbb226b4f4dbeee9e2455
khalilBahi <7753@holbertonstudents.com>
2024-02-05 10:49:06 +0100

    prints a square with the character #

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -18,11 +18,11 @@ def print_square(size):
     if not isinstance(size, (int, float)):
         raise TypeError("size must be an integer")
 
-    if type(size) is float and size <= 0:
+    if type(size) is float and size < 0:
         raise TypeError("size must be an integer")
 
     if size < 0:
         raise ValueError("size must be >= 0")
 
-    for i in range(size):
-        print("#" * size)
+    for i in range(int(size)):
+        print("#" * int(size))
