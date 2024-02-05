ba43ba8f4ed9b66b5255164c691b9a3830138f5b
khalilBahi <7753@holbertonstudents.com>
2024-02-05 15:56:50 +0100

    prints a text with 2 new lines after each of these characters: ., ? and :

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -8,9 +8,9 @@ def text_indentation(text):
     """
     Format the input text by adding 2 newlines after (:?.)
         Args:
-            text (str): The input text to be formatted.
+            text (str): The input text to be formatted
         Raises:
-            TypeError: If the 'text' argument is not a string.
+            TypeError: If the 'text' argument is not a string
     """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
