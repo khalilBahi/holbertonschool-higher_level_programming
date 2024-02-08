cca72d6cde6f350eb0df25e5150bd70dbbf72615
khalilBahi <7753@holbertonstudents.com>
2024-02-08 18:13:36 +0100

    Write a class Rectangle that defines a rectangle by: (based on 3-rectangle.py)

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -2,8 +2,9 @@
 """ module rectangle """
 
 
-class Rectangle():
-    """ create new rectangle """
+class Rectangle:
+    """create new rectangle"""
+
     def __init__(self, width=0, height=0):
 
         self.width = width
@@ -40,9 +41,11 @@ class Rectangle():
         if self.__width == 0 or self.__height == 0:
             return 0
         return (self.__width + self.__height) * 2
+
     def __str__(self):
         if self.__width == 0 or self.__height == 0:
             return ""
         return "\n".join(["#" * self.__width] * self.__height)
+
     def __repr__(self):
-        return f"Rectangle({self.__width}, {self.__height})"
+        return "Rectangle({}, {})".format(self.__width, self.__height)
