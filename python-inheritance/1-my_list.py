3e3338a2325829bfb60e56d83a435ec328eb73a4
khalilBahi <7753@holbertonstudents.com>
2024-02-10 19:15:42 +0100

    sort the list and print it

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -3,9 +3,12 @@
 """
 module my list
 """
+
+
 class MyList(list):
-    """ create a list """
+    """create a list"""
+
     def print_sorted(self):
-        """sort the list and print it """
+        """sort the list and print it"""
         my_list = sorted(self)
         print(my_list)
