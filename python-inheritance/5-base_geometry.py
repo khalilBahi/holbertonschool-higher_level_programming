2d7f1cd27120fa0f38157066a6ca2c2d0ea70243
khalilBahi <7753@holbertonstudents.com>
2024-02-10 21:17:03 +0100

    Write an empty class BaseGeometry

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -4,6 +4,10 @@
 This module provides a function to look up
 attributes and methods of an object.
 """
+
+
 class BaseGeometry:
-      """ create empty class"""
+    """create empty class"""
+
+
 pass
