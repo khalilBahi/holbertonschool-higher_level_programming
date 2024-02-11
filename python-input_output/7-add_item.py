d354c1965352e8aa1d45e898702026fe01c5d7b2
khalilBahi <7753@holbertonstudents.com>
2024-02-11 20:19:56 +0100

    adds all arguments to a Python list, and then save them to a file

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 """ python input output """
+import sys
+
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
-
-
-import json
-import sys
 
 
 arguments = sys.argv[1:]
