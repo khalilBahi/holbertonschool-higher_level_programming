39ee11ea0a092050ad1a0c829147eba792f4136f
khalilBahi <7753@holbertonstudents.com>
2024-02-17 15:53:49 +0100

    Validate attributes

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -19,11 +19,17 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """
+        Get the width of the rectangle
+        """
         return self.__width
 
     @width.setter
     def width(self, value):
-        if type(value) is not int:
+        """
+        Set the width of the rectangle
+        """
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
@@ -31,11 +37,17 @@ class Rectangle(Base):
 
     @property
     def height(self):
+        """
+        Get the height of the rectangle
+        """
         return self.__height
 
     @height.setter
     def height(self, value):
-        if type(value) is not int:
+        """
+        Set the height of the rectangle
+        """
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
@@ -43,24 +55,36 @@ class Rectangle(Base):
 
     @property
     def x(self):
+        """
+        Get the x
+        """
         return self.__x
 
     @x.setter
     def x(self, value):
-        if type(value) is not int:
+        """
+        Set the x
+        """
+        if not isinstance(value, int):
             raise TypeError("x must be an integer")
         if value < 0:
-            raise ValueError("x must be > 0")
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
     def y(self):
+        """
+        Get the y
+        """
         return self.__y
 
     @y.setter
     def y(self, value):
-        if type(value) is not int:
+        """
+        Set the y
+        """
+        if not isinstance(value, int):
             raise TypeError("y must be an integer")
         if value < 0:
-            raise ValueError("y must be > 0")
+            raise ValueError("y must be >= 0")
         self.__y = value
