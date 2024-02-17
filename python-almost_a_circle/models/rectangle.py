4494d14c39d95394387e1fd3d7bb6e1ee42fbf92
khalilBahi <7753@holbertonstudents.com>
2024-02-17 16:03:01 +0100

    ;

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -94,5 +94,6 @@ class Rectangle(Base):
         return self.__width * self.__height
 
     def display(self):
+        """print the rectangle with #"""
         for i in range(self.height):
             print("#" * self.width)
