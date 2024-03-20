8fe50c34954dc79a855e6441d70b50695044ff29
khalilBahi <7753@holbertonstudents.com>
2024-03-20 22:17:18 +0100

    Cities by States

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,4 @@
 -- Write a script that lists all cities contained in the database hbtn_0d_usa.
 SELECT cities.id , cities.name , states.name
     FROM cities
-    JOIN hbtn_0d_usa.states ON cities.state_id = states.id
-    ORDER BY cities.id ASC;
+    INNER JOIN states ON states.id = cities.state_id;
