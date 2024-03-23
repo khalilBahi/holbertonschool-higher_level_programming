73597840f486b54be39e109545210e73a0c75274
khalilBahi <7753@holbertonstudents.com>
2024-03-23 14:57:44 +0100

    Filter states

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Script that lists all states from the database hbtn_0e_0_usa.
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    """
+    Main function to connect to the MySQL server and fetch states data.
+    """
+
+    username, password, database = sys.argv[1:4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE state_name LIKE 'N%' ORDER BY id")
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row) 
